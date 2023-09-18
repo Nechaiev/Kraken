4a82014b2c3fd76f20a8819c7bcece1c2cec9783
Stanislav <stanislav.nechaev@gmail.com>
2023-09-19 01:16:21 +0300

    add html card

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,131 @@
 <script setup></script>
 
 <template>
-  <div class="text-5xl">hello world</div>
+  <div class="text-caption">hello world</div>
+  <div class="flex flex-col gap-5 container mx-auto">
+    <ul class="flex flex-wrap gap-8">
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-gradient"></div>
+        <div class="">Gradient</div>
+      </li>
+    </ul>
+
+    <ul class="flex flex-wrap gap-8">
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-blue"></div>
+        <div class="">Blue</div>
+        <div class="">#DBE6ED</div>
+      </li>
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-brandBlue"></div>
+        <div class="">Brand Blue</div>
+        <div class="">#4288B5</div>
+      </li>
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-blueSecond"></div>
+        <div class="">Blue 2</div>
+        <div class="">#072E53</div>
+      </li>
+    </ul>
+
+    <ul class="flex flex-wrap gap-8">
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-purple"></div>
+        <div class="">Purple</div>
+        <div class="">#F4DCFF</div>
+      </li>
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-brandPurple"></div>
+        <div class="">brand Purple</div>
+        <div class="">#BA90CE</div>
+      </li>
+    </ul>
+
+    <ul class="flex flex-wrap gap-8">
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-red"></div>
+        <div class="">red</div>
+        <div class="">#F2E1E0</div>
+      </li>
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-redSecond"></div>
+        <div class="">red 2</div>
+        <div class="">#9B1C13</div>
+      </li>
+    </ul>
+
+    <ul class="flex flex-wrap gap-8">
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-green"></div>
+        <div class="">Green</div>
+        <div class="">#D1E7DD</div>
+      </li>
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-greenSecond"></div>
+        <div class="">green 2</div>
+        <div class="">#0F5132</div>
+      </li>
+    </ul>
+
+    <ul class="flex flex-wrap gap-8">
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-gold"></div>
+        <div class="">Gold</div>
+        <div class="">#FFF3CD</div>
+      </li>
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-goldSecond"></div>
+        <div class="">Gold 2</div>
+        <div class="">#664D03</div>
+      </li>
+    </ul>
+
+    <ul class="flex flex-wrap gap-8">
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-light"></div>
+        <div class="">Light</div>
+        <div class="">#FFFFFF</div>
+      </li>
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-gray"></div>
+        <div class="">Gray</div>
+        <div class="">#F6F6FA</div>
+      </li>
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-graySecond"></div>
+        <div class="">gray 2</div>
+        <div class="">#E9E9E9</div>
+      </li>
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-grayFourth"></div>
+        <div class="">gray 3</div>
+        <div class="">#323232</div>
+      </li>
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-dark"></div>
+        <div class="">Dark</div>
+        <div class="">#0E3046</div>
+      </li>
+    </ul>
+
+    <ul class="flex flex-wrap gap-8">
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-dark"></div>
+        <div class="">Dark</div>
+        <div class="">#0E3046</div>
+      </li>
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-darkSecond"></div>
+        <div class="">Dark 2</div>
+        <div class="">#202637</div>
+      </li>
+      <li class="">
+        <div class="w-[100px] h-[100px] bg-darkThird"></div>
+        <div class="">Dark 3</div>
+        <div class="">#001223</div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
