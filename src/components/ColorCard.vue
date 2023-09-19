07c26d1a20a7909bd4f42b4397b2136a5647c597
Stanislav <stanislav.nechaev@gmail.com>
2023-09-19 22:28:43 +0300

    add component ColorCard and props

---
+++ src/components/ColorCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="">
+    <div class="w-[100px] h-[100px]" :class="classColor"></div>
+    <div class="">{{ title }}</div>
+    <div class="">{{ description }}</div>
+  </li>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  classColor: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    validator(value) {
+      return value.length >= 6;
+    },
+  },
+});
+</script>
+
+<style></style>
