917b3bd8e35a3007d003a2cfd0aef79da70dc81e
Stanislav <stanislav.nechaev@gmail.com>
2023-09-19 22:27:32 +0300

    refactoring to component

--- src/App.vue
+++ src/App.vue
@@ -1,131 +1,100 @@
-<script setup></script>
-
 <template>
   <div class="text-6xl">hello world</div>
   <div class="container bg-slate-100 flex flex-col gap-8 mx-auto">
     <ul class="flex flex-wrap gap-8">
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-gradient"></div>
-        <div class="">Gradient</div>
-      </li>
+      <ColorCard title="Gradient" classColor="bg-gradient" />
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-blue"></div>
-        <div class="">Blue</div>
-        <div class="">#DBE6ED</div>
-      </li>
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-brandBlue"></div>
-        <div class="">Brand Blue</div>
-        <div class="">#4288B5</div>
-      </li>
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-blueSecond"></div>
-        <div class="">Blue 2</div>
-        <div class="">#072E53</div>
-      </li>
+      <ColorCard title="Blue" classColor="bg-blue" description="#DBE6ED" />
+      <ColorCard
+        title="Brand Blue"
+        classColor="bg-brandBlue"
+        description="#4288B5"
+      />
+      <ColorCard
+        title="Blue 2"
+        classColor="bg-blueSecond"
+        description="#072E53"
+      />
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-purple"></div>
-        <div class="">Purple</div>
-        <div class="">#F4DCFF</div>
-      </li>
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-brandPurple"></div>
-        <div class="">brand Purple</div>
-        <div class="">#BA90CE</div>
-      </li>
+      <ColorCard title="Purple" classColor="bg-purple" description="#F4DCFF" />
+      <ColorCard
+        title="brand Purple"
+        classColor="bg-brandPurple"
+        description="#BA90CE"
+      />
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-red"></div>
-        <div class="">red</div>
-        <div class="">#F2E1E0</div>
-      </li>
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-redSecond"></div>
-        <div class="">red 2</div>
-        <div class="">#9B1C13</div>
-      </li>
+      <ColorCard title="red" classColor="bg-red" description="#F2E1E0" />
+      <ColorCard
+        title="red 2"
+        classColor="bg-redSecond"
+        description="#9B1C13"
+      />
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-green"></div>
-        <div class="">Green</div>
-        <div class="">#D1E7DD</div>
-      </li>
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-greenSecond"></div>
-        <div class="">green 2</div>
-        <div class="">#0F5132</div>
-      </li>
+      <ColorCard title="Green" classColor="bg-green" description="#D1E7DD" />
+      <ColorCard
+        title="green 2"
+        classColor="bg-greenSecond"
+        description="#0F5132"
+      />
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-gold"></div>
-        <div class="">Gold</div>
-        <div class="">#FFF3CD</div>
-      </li>
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-goldSecond"></div>
-        <div class="">Gold 2</div>
-        <div class="">#664D03</div>
-      </li>
+      <ColorCard title="Gold" classColor="bg-gold" description="#FFF3CD" />
+      <ColorCard
+        title="Gold 2"
+        classColor="bg-goldSecond"
+        description="#664D03"
+      />
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-light"></div>
-        <div class="">Light</div>
-        <div class="">#FFFFFF</div>
-      </li>
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-gray"></div>
-        <div class="">Gray</div>
-        <div class="">#F6F6FA</div>
-      </li>
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-graySecond"></div>
-        <div class="">gray 2</div>
-        <div class="">#E9E9E9</div>
-      </li>
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-grayFourth"></div>
-        <div class="">gray 3</div>
-        <div class="">#323232</div>
-      </li>
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-dark"></div>
-        <div class="">Dark</div>
-        <div class="">#0E3046</div>
-      </li>
+      <ColorCard title="Light" classColor="bg-light" description="#FFFFFF" />
+      <ColorCard title="Gray" classColor="bg-gray" description="#F6F6FA" />
+      <ColorCard
+        title="gray 2"
+        classColor="bg-graySecond"
+        description="#E9E9E9"
+      />
+      <ColorCard
+        title="gray 3"
+        classColor="bg-grayThird"
+        description="#989898"
+      />
+      <ColorCard
+        title="gray 4"
+        classColor="bg-grayFourth"
+        description="#323232"
+      />
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-dark"></div>
-        <div class="">Dark</div>
-        <div class="">#0E3046</div>
-      </li>
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-darkSecond"></div>
-        <div class="">Dark 2</div>
-        <div class="">#202637</div>
-      </li>
-      <li class="">
-        <div class="w-[100px] h-[100px] bg-darkThird"></div>
-        <div class="">Dark 3</div>
-        <div class="">#001223</div>
-      </li>
+      <ColorCard title="Dark" classColor="bg-dark" description="#0E3046" />
+      <ColorCard
+        title="Dark 2"
+        classColor="bg-darkSecond"
+        description="#202637"
+      />
+      <ColorCard
+        title="Dark 3"
+        classColor="bg-darkThird"
+        description="#001223"
+      />
     </ul>
   </div>
 </template>
+
+<script setup>
+
+import ColorCard from "@/components/ColorCard.vue";
+
+</script>
 
 <style scoped></style>
