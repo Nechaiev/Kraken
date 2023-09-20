f2e329da105364f239401c49633402afb106325b
Stanislav <stanislav.nechaev@gmail.com>
2023-09-20 11:42:33 +0300

    add theme changer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="text-6xl">hello world</div>
-  <div class="container bg-slate-100 flex flex-col gap-8 mx-auto">
+
+  <div
+    :class="containerClasses"
+    class="container bg-slate-100 flex flex-col gap-8 mx-auto"
+  >
+    <div class="mb-6 flex flex-wrap gap-3">
+      <label v-for="theme in themes" :key="theme" class="flex flex-wrap gap-2">
+        <input
+          type="radio"
+          :checked="theme === selectedTheme"
+          name="theme"
+          @click="onSetTheme(theme)"
+          value="theme"
+        />
+        <span>{{ theme }}</span>
+      </label>
+    </div>
     <ul class="flex flex-wrap gap-8">
-      <ColorCard title="Gradient" classColor="bg-gradient" />
+      <color-card title="Gradient" classColor="bg-gradient" />
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <ColorCard title="Blue" classColor="bg-blue" description="#DBE6ED" />
-      <ColorCard
+      <color-card title="Blue" classColor="bg-blue" description="#DBE6ED" />
+      <color-card
         title="Brand Blue"
         classColor="bg-brandBlue"
         description="#4288B5"
       />
-      <ColorCard
+      <color-card
         title="Blue 2"
         classColor="bg-blueSecond"
         description="#072E53"
@@ -20,8 +36,8 @@
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <ColorCard title="Purple" classColor="bg-purple" description="#F4DCFF" />
-      <ColorCard
+      <color-card title="Purple" classColor="bg-purple" description="#F4DCFF" />
+      <color-card
         title="brand Purple"
         classColor="bg-brandPurple"
         description="#BA90CE"
@@ -29,8 +45,8 @@
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <ColorCard title="red" classColor="bg-red" description="#F2E1E0" />
-      <ColorCard
+      <color-card title="red" classColor="bg-red" description="#F2E1E0" />
+      <color-card
         title="red 2"
         classColor="bg-redSecond"
         description="#9B1C13"
@@ -38,8 +54,8 @@
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <ColorCard title="Green" classColor="bg-green" description="#D1E7DD" />
-      <ColorCard
+      <color-card title="Green" classColor="bg-green" description="#D1E7DD" />
+      <color-card
         title="green 2"
         classColor="bg-greenSecond"
         description="#0F5132"
@@ -47,8 +63,8 @@
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <ColorCard title="Gold" classColor="bg-gold" description="#FFF3CD" />
-      <ColorCard
+      <color-card title="Gold" classColor="bg-gold" description="#FFF3CD" />
+      <color-card
         title="Gold 2"
         classColor="bg-goldSecond"
         description="#664D03"
@@ -56,19 +72,19 @@
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <ColorCard title="Light" classColor="bg-light" description="#FFFFFF" />
-      <ColorCard title="Gray" classColor="bg-gray" description="#F6F6FA" />
-      <ColorCard
+      <color-card title="Light" classColor="bg-light" description="#FFFFFF" />
+      <color-card title="Gray" classColor="bg-gray" description="#F6F6FA" />
+      <color-card
         title="gray 2"
         classColor="bg-graySecond"
         description="#E9E9E9"
       />
-      <ColorCard
+      <color-card
         title="gray 3"
         classColor="bg-grayThird"
         description="#989898"
       />
-      <ColorCard
+      <color-card
         title="gray 4"
         classColor="bg-grayFourth"
         description="#323232"
@@ -76,13 +92,13 @@
     </ul>
 
     <ul class="flex flex-wrap gap-8">
-      <ColorCard title="Dark" classColor="bg-dark" description="#0E3046" />
-      <ColorCard
+      <color-card title="Dark" classColor="bg-dark" description="#0E3046" />
+      <color-card
         title="Dark 2"
         classColor="bg-darkSecond"
         description="#202637"
       />
-      <ColorCard
+      <color-card
         title="Dark 3"
         classColor="bg-darkThird"
         description="#001223"
@@ -92,9 +108,23 @@
 </template>
 
 <script setup>
-
 import ColorCard from "@/components/ColorCard.vue";
+import { ref, computed } from "vue";
 
+const containerClasses = computed(() => {
+  const classes = [];
+  if (selectedTheme.value === "secondary") {
+    classes.push("theme-secondary");
+  }
+  return classes.join(" ");
+});
+
+const themes = ["default", "secondary"];
+const selectedTheme = ref("default");
+
+const onSetTheme = (newTheme) => {
+  selectedTheme.value = newTheme;
+};
 </script>
 
 <style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: 'Poppins, sans-serif'
+    },
     extend: {
       colors: {
         gradient: 'var(--gradient)',
