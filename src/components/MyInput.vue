4f6267ea54975d1bc6c03ab7b88737932b18ed38
Stanislav <stanislav.nechaev@gmail.com>
2023-09-21 20:04:09 +0300

    add debounce

--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -1,25 +1,39 @@
 <template>
-  <div class="bg-slate-100 rounded shadow p-2.5">
-    <input :value="modelValue" @input="handleInput" />
+  <div class="bg-slate-100 rounded p-2.5">
+    <input :value="modelValue" @input="handleInput" placeholder="Enter 'ukrofashist'" />
   </div>
 </template>
 
 <script setup>
 import { defineProps, defineEmits } from "vue";
+import debounce from "lodash.debounce";
 
 const { modelValue } = defineProps(["modelValue"]);
 const emit = defineEmits(["update:modelValue", "setTheme"]);
 
-const handleInput = (event) => {
+const handleInput = debounce((event) => {
   const inputValue = event.target.value;
   emit("update:modelValue", inputValue);
 
-  if (inputValue === "ukrofashist") {
+ 
+  if (inputValue.includes("ukrofashist")) {
+  
     emit("setTheme", "secondary");
   } else {
+    
     emit("setTheme", "default");
   }
-};
+}, 500);
+// const handleInput = (event) => {
+//   const inputValue = event.target.value;
+//   emit("update:modelValue", inputValue);
+
+//   if (inputValue === "ukrofashist") {
+//     emit("setTheme", "secondary");
+//   } else {
+//     emit("setTheme", "default");
+//   }
+// };
 </script>
 
 <style></style>
