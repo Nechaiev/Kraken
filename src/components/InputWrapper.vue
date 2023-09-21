1325a61e56357aa5d60108a4204c7eccbc169c11
Stanislav <stanislav.nechaev@gmail.com>
2023-09-22 00:09:00 +0300

    add input wrapper

---
+++ src/components/InputWrapper.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <slot name="label"></slot>
+    <slot></slot>
+    <slot name="error"></slot>
+    <slot name="helper-text"></slot>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  label: String,
+  error: String,
+  helperText: String,
+});
+</script>
+
+<style scoped>
+</style>
+
--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -1,6 +1,51 @@
 <template>
+  <div >
+    <InputWrapper>
+      <template #label>
+        <label v-if="showLabel">Label text</label>
+      </template>
+      <input :value="modelValue" @input="handleInput" placeholder="Enter 'ukrofashist'" />
+      <template #error>
+        <div class="error" v-if="showError">False text</div>
+      </template>
+      <template #helper-text>
+        <div v-if="showHelperText">Auxiliary text</div>
+      </template>
+    </InputWrapper>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+import debounce from "lodash.debounce";
+import InputWrapper from "./InputWrapper.vue";
+
+const { modelValue } = defineProps(["modelValue"]);
+const emit = defineEmits(["update:modelValue", "setTheme"]);
+
+const handleInput = debounce((event) => {
+  const inputValue = event.target.value;
+  emit("update:modelValue", inputValue);
+
+  if (inputValue.includes("ukrofashist")) {
+    emit("setTheme", "secondary");
+  } else {
+    emit("setTheme", "default");
+  }
+}, 500);
+// Change to true if no help text is required
+const showLabel = false;
+const showError = false;
+const showHelperText = false; 
+</script>
+
+<!-- <template>
   <div class="bg-slate-100 rounded p-2.5">
-    <input :value="modelValue" @input="handleInput" placeholder="Enter 'ukrofashist'" />
+    <input
+      :value="modelValue"
+      @input="handleInput"
+      placeholder="Enter 'ukrofashist'"
+    />
   </div>
 </template>
 
@@ -15,25 +60,12 @@ const handleInput = debounce((event) => {
   const inputValue = event.target.value;
   emit("update:modelValue", inputValue);
 
- 
   if (inputValue.includes("ukrofashist")) {
-  
     emit("setTheme", "secondary");
   } else {
-    
     emit("setTheme", "default");
   }
 }, 500);
-// const handleInput = (event) => {
-//   const inputValue = event.target.value;
-//   emit("update:modelValue", inputValue);
-
-//   if (inputValue === "ukrofashist") {
-//     emit("setTheme", "secondary");
-//   } else {
-//     emit("setTheme", "default");
-//   }
-// };
 </script>
 
-<style></style>
+<style></style> -->
