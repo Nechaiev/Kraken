692ef7a28eed8d5d2bdb7d87295f8be033c2c3b8
Stanislav <stanislav.nechaev@gmail.com>
2023-09-26 02:23:31 +0300

    moved the loading into a separate component

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -3,15 +3,7 @@
       :is="props.as"
       :disabled="props.disabled"
       :class="buttonClass">
-
-    <svg v-if="props.loading" class="animate-spin h-5 w-5 absolute"
-         xmlns="http://www.w3.org/2000/svg"
-         fill="none"
-         viewBox="0 0 24 24">
-      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-      <path class="opacity-75" fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-    </svg>
+    <my-loader v-if="props.loading" />
 
     <component
         :is="props.leftIcon"
@@ -36,16 +28,17 @@
 </template>
 
 <script setup>
-import {defineProps, computed} from "vue";
-import { cva } from "class-variance-authority";
+import {computed, defineProps} from "vue";
+import {cva} from "class-variance-authority";
+import MyLoader from "@/components/UI/fields/MyLoader.vue";
 
 const buttonClass = computed(() => {
   return cva("inline-flex justify-center items-center text-sm rounded min-h-[32px] px-3 py-0.5 font-semibold ", {
     variants: {
       intent: {
-        primary: "bg-black text-white hover:bg-gray-800",
+        primary: "bg-black text-white hover:bg-sky-700 ",
         secondary: "bg-black/5 hover:bg-black/10 text-gray-600",
-        danger: "bg-redSecond text-white hover:bg-red-500",
+        danger: "bg-pink-500 text-white hover:bg-pink-300",
         text: "text-gray-700 hover:bg-black/5"
       },
       disabled: {
