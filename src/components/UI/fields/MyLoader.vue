6817c55f00c8cde547d94782cf76f76cd1f62eb0
Stanislav <stanislav.nechaev@gmail.com>
2023-09-26 21:55:37 +0300

    cleaned up empty and redundant terms

--- src/components/UI/fields/MyLoader.vue
+++ src/components/UI/fields/MyLoader.vue
@@ -1,7 +1,3 @@
-<script setup>
-
-</script>
-
 <template>
   <svg class="animate-spin h-5 w-5 absolute"
        xmlns="http://www.w3.org/2000/svg"
@@ -12,7 +8,3 @@
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
   </svg>
 </template>
-
-<style scoped>
-
-</style>
